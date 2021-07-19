ad03e67d6e4da2429e3dae34f6325739f2c6aec3
tharinda <tharindahp@gmail.com>
2021-07-19 18:28:20 +0530

    created item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,3 +1,21 @@
-const Item = () => {};
+import "../components/Item.css";
+
+const Item = () => {
+  return (
+    <div className="item">
+      <div className="item__content">
+        <img
+          src={"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"}
+          alt={"Item Image"}
+        />
+        <div className="info">
+          <h3>Nice Backpack</h3>
+          <p>$ 110</p>
+        </div>
+      </div>
+      <button>Add to Cart</button>
+    </div>
+  );
+};
 
 export default Item;
