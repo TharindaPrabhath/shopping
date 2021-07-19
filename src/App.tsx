78b5a35b5d94c112b99469085452292a1d88a80b
tharinda <tharindahp@gmail.com>
2021-07-19 18:29:48 +0530

    added rout paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Nav from "./components/Nav";
+import CartScreen from "./screens/CartScreen";
+import MainScreen from "./screens/MainScreen";
+
+import "../src/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <Router>
+      <header className={"nav"}>
+        <Nav />
+      </header>
+      <body>
+        <div className="content">
+          <Route path={"/"} exact component={MainScreen} />
+          <Route path={"/cart"} component={CartScreen} />
+        </div>
+      </body>
+    </Router>
   );
 }
 
